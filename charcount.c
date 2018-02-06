dd08e38335b570778469fa8ad27af6c4bf845a61
sowbarnigavsb <31504504+sowbarnigavsb@users.noreply.github.com>
2018-02-06 14:55:26 +0530

    Update charcount.c

--- charcount.c
+++ charcount.c
@@ -1,16 +1,21 @@
+
 #include <stdio.h>
 
 int main(void) {
 	int i,count=0;
 	char s[100];
-	scanf("%s",&s);
+            gets(s);
 	for(i=0;s[i]!='\0';i++)
 	{
-		if((s[i]>='a'||s[i]<='z')||(s[i]>='A'||s[i]<='Z'))
+		if((s[i]>='a'&&s[i]<='z')||(s[i]>='A'&&s[i]<='Z'))
 		count++;
-		if(s[i]==' ')
-			count--;
-	}
+		if(s[i]=='!'||s[i]=='@'||s[i]=='#'||s[i]=='$'||s[i]=='%'||s[i]=='^')
+		count++;
+		if(s[i]=='&'||s[i]=='*'||s[i]=='('||s[i]==')'||s[i]=='-'||s[i]=='+')
+		count++;
+		if(s[i]>='0'&&s[i]<='9')
+		count++;
+		}
 	printf("%d",count);
 	
 	// your code goes here
