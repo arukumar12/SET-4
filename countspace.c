b74d9040f5b46d69e68ffb7cf01c57402350cf51
sowbarnigavsb <31504504+sowbarnigavsb@users.noreply.github.com>
2018-02-06 15:02:03 +0530

    Create countspace.c

---
+++ countspace.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int main(void) {
+	int i,count=0;
+	char s[100];
+            gets(s);
+	for(i=0;s[i]!='\0';i++)
+	{
+	if(s[i]==' ')
+	count++;
+		}
+	printf("%d",count);
+	
+	// your code goes here
+	return 0;
+}
